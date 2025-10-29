e934b2197e382c73755959eceaef6d42e79817df
swisslson <cyril.hany@gmail.com>
2025-10-29 13:15:44 +0100

    prepa pull dernier version

---
+++ Sources/BonheurApp/Controllers/souvenirDefiContoller.swift
@@ -0,0 +1,15 @@
+//
+//  souvenirDefiContoller.swift
+//  BonheurApp
+//
+//  Created by cyrilH on 29/10/2025.
+//
+
+//import Fluent
+//import Vapor
+//
+//struct souvenirDefiController: RouteCollection{
+//    func boot(routes: any RoutesBuilder) throws{
+//        let souvenirDefi = routes.grouped(<#T##path: PathComponent...##PathComponent#>)
+//    }
+
