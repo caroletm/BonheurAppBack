519d6d8a0dc93e5ba61ead78f62be795e85c74e3
swisslson <cyril.hany@gmail.com>
2025-11-03 10:22:43 +0100

    mis les import

--- Sources/BonheurApp/Middleware/CORSMiddleware.swift
+++ Sources/BonheurApp/Middleware/CORSMiddleware.swift
@@ -5,7 +5,8 @@
 //  Created by cyrilH on 03/11/2025.
 //
 
-
+import Gatekeeper
+import Vapor
 let corsConfiguration = CORSMiddleware.Configuration(
     allowedOrigin: .all,
     allowedMethods: [.GET, .POST, .PUT, .DELETE, .OPTIONS],
